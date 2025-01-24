0a03ab941b6eec7fc0ab7d35db18a5c134ce57ed
Carlos Rivera <carlosrdev@gmail.com>
2025-01-24 13:59:50 -0600

    Init

---
+++ src/test/java/org/example/rspublicaciones/RsPublicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rspublicaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsPublicacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
